8a1e72489481ee8c236d673eaf5bacc5634b2251
Zihan Zhao <zihan0932@gmail.com>
2022-10-29 07:39:00 -0700

    Add render error message function

--- scripts/views/view.js
+++ scripts/views/view.js
@@ -44,33 +44,13 @@ export default class View {
     this._parentElement.innerHTML = "";
   }
 
-  // renderError(message = this._errorMessage) {
-  //   const markup = `
-  //     <div class="error">
-  //       <div>
-  //         <svg>
-  //           <use href="${icons}#icon-alert-triangle"></use>
-  //         </svg>
-  //       </div>
-  //       <p>${message}</p>
-  //     </div>
-  //   `;
-  //   this._clear();
-  //   this._parentElement.insertAdjacentHTML("afterbegin", markup);
-  // }
-
-  // renderMessage(message = this._message) {
-  //   const markup = `
-  //     <div class="message">
-  //       <div>
-  //         <svg>
-  //           <use href="${icons}#icon-smile"></use>
-  //         </svg>
-  //       </div>
-  //       <p>${message}</p>
-  //     </div>
-  //   `;
-  //   this._clear();
-  //   this._parentElement.insertAdjacentHTML("afterbegin", markup);
-  // }
+  renderError(message = this._errorMessage) {
+    const markup = `
+      <div class="error">
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
 }
